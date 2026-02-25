2636cd63804741d649c881b2adc341efe4ad4166
Teamarin <marinteo@students.zhaw.ch>
2026-02-25 10:21:50 +0100

    email adressen hinzugefügt

--- views/home.py
+++ views/home.py
@@ -6,8 +6,8 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Teodora Marinkovic (marinteo@students.zhaw.ch)
+- Yakin Boumaiza (boumayak@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
